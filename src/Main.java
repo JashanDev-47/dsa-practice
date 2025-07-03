f95d36663a9ee512b963c5b6d3d0d79781759f65
nikku <jashanpc3600@gmail.com>
2025-07-03 18:14:28 +0530

    The rightmost set bit Finded

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 
 
 
-
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int n = 52;
+        int ans = rightMostSetBit(n);
+        System.out.println("The ans is : " + ans);
+    }
+
+    private static int rightMostSetBit(int n) {
+        int bitmask = ~n + 1;
+
+        return n & bitmask;
     }
 }
